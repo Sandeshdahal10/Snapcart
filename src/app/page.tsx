5242ff1cab9267bce1ec854a8bcbe655f8d3a912
Sandesh Dahal <sandeshdahal3547@gmail.com>
2025-12-25 07:17:21 +0545

    Mount Nav component to Home page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { auth } from "@/auth";
 import EditRoleMobile from "@/components/EditRoleMobile";
+import Nav from "@/components/Nav";
 import connectDb from "@/lib/db";
 import User from "@/models/user.model";
 import { redirect } from "next/navigation";
@@ -18,5 +19,9 @@ export default async function Home() {
     return <EditRoleMobile />;
   }
 
-  return <div></div>;
+  return (
+    <>
+      <Nav />
+    </>
+  );
 }
