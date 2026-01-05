6ed61c9b672a2652b770c1dfc85991503f4483e2
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-05 16:35:48 +0545

    Completed Nav Bar design.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,13 @@
 "use client";
-import { LogOut, Package, Search, ShoppingCartIcon, User } from "lucide-react";
+import {
+  Cross,
+  LogOut,
+  Package,
+  Search,
+  ShoppingCartIcon,
+  User,
+  X,
+} from "lucide-react";
 import mongoose from "mongoose";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
@@ -17,30 +25,34 @@ interface IUser {
 }
 function Nav({ user }: { user: IUser }) {
   const [open, setOpen] = useState(false);
-  const profileDropDown=useRef<HTMLDivElement>(null)
-  useEffect(()=>{
-    const handleClickOutside=(e:MouseEvent)=>{
-      if(profileDropDown.current && !profileDropDown.current.contains(e.target as Node)){
-        setOpen(false)
+  const profileDropDown = useRef<HTMLDivElement>(null);
+  const [searchBarOpen, setSearchBarOpen] = useState(false);
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        profileDropDown.current &&
+        !profileDropDown.current.contains(e.target as Node)
+      ) {
+        setOpen(false);
       }
-    }
-    document.addEventListener("mousedown",handleClickOutside)
-    return ()=>document.removeEventListener("mousedown",handleClickOutside)
-  },[])
-//   Flow of the use Effect 
-//   Component load huncha
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+  //   Flow of the use Effect
+  //   Component load huncha
 
-// Dropdown element ref ma save huncha
+  // Dropdown element ref ma save huncha
 
-// Page ma click listener add huncha
+  // Page ma click listener add huncha
 
-// User click garchha:
+  // User click garchha:
 
-// dropdown bhitra → ignore
+  // dropdown bhitra → ignore
 
-// dropdown bahira → close
+  // dropdown bahira → close
 
-// Component hatauda listener clean huncha
+  // Component hatauda listener clean huncha
   return (
     <div className="w-[95%] fixed top-4 left-1/2 -translate-x-1/2 bg-linear-to-r from-green-500 to-green-700 rounded-2xl shadow-lg shadow-black/30 flex justify-between items-center h-20 px-4 md:px-8 z-50">
       <Link
@@ -58,6 +70,13 @@ function Nav({ user }: { user: IUser }) {
         />
       </form>
       <div className="flex items-center gap-3 md:gap-6 relative">
+        <div
+          className="bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition md:hidden"
+          onClick={() => setSearchBarOpen((prev) => !prev)}
+        >
+          <Search className="text-green-600 w-6 h-6" />
+        </div>
+
         <Link
           href={""}
           className="relative bg-white w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition rounded-full"
@@ -98,37 +117,79 @@ function Nav({ user }: { user: IUser }) {
                   y: -10,
                   scale: 0.95,
                 }}
-              className="absolute right-0 mt-3 w-56 bg-white rounded-2xl shadow-xl border border-gray-200 p-3 z-999"
+                className="absolute right-0 mt-3 w-56 bg-white rounded-2xl shadow-xl border border-gray-200 p-3 z-999"
               >
                 <div className="flex items-center gap-3 px-3 py-2 border-b border-gray-100">
                   <div className="w-10 h-10 rounded-full bg-green-100 flex items-center justify-center overflow-hidden relative">
-                    {user.image ? 
-              <Image
-                src={user.image}
-                alt="user"
-                fill
-                className="object-cover rounded-full"
-              />
-             : 
-              <User/>
-            }
+                    {user.image ? (
+                      <Image
+                        src={user.image}
+                        alt="user"
+                        fill
+                        className="object-cover rounded-full"
+                      />
+                    ) : (
+                      <User />
+                    )}
                   </div>
                   <div>
-                    <div className="text-gray-800 font-semibold">{user.name}</div>
-                    <div className="text-xs text-gray-500 capitalize">{user.role}</div>
+                    <div className="text-gray-800 font-semibold">
+                      {user.name}
+                    </div>
+                    <div className="text-xs text-gray-500 capitalize">
+                      {user.role}
+                    </div>
                   </div>
                 </div>
 
-                <Link href={""} className="flex items-center gap-2 px-3 py-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium" onClick={()=>setOpen(false)}>
-                <Package className="w-5 h-5 text-green-600"/>
-                My Orders
+                <Link
+                  href={""}
+                  className="flex items-center gap-2 px-3 py-3 hover:bg-green-50 rounded-lg text-gray-700 font-medium"
+                  onClick={() => setOpen(false)}
+                >
+                  <Package className="w-5 h-5 text-green-600" />
+                  My Orders
                 </Link>
-                <button className="flex items-center gap-2 w-full text-left px-3 py-3 hover:bg-red-50 rounded-lg text-gray-700 font-medium" onClick={()=>{
-                  setOpen(false)
-                  signOut({callbackUrl:"/login"})
-                }}>
-                  <LogOut className="w-5 h-5 text-red-600"/>
+                <button
+                  className="flex items-center gap-2 w-full text-left px-3 py-3 hover:bg-red-50 rounded-lg text-gray-700 font-medium"
+                  onClick={() => {
+                    setOpen(false);
+                    signOut({ callbackUrl: "/login" });
+                  }}
+                >
+                  <LogOut className="w-5 h-5 text-red-600" />
                   Log Out
+                </button>
+              </motion.div>
+            )}
+          </AnimatePresence>
+          <AnimatePresence>
+            {searchBarOpen && (
+              <motion.div
+                initial={{
+                  opacity: 0,
+                  y: -10,
+                  scale: 0.95,
+                }}
+                animate={{ opacity: 1, y: 0, scale: 1 }}
+                transition={{ duration: 0.4 }}
+                exit={{
+                  opacity: 0,
+                  y: -10,
+                  scale: 0.95,
+                }}
+                className="fixed top-24 left-1/2 -translate-x-1/2 w-[90%] bg-white rounded-full shadow-lg z-40 flex items-center px-4 py-2"
+              >
+                <Search className="text-gray-500 w-5 h-5 mr-2" />
+                <form className="grow">
+                  <input
+                    type="text"
+                    className="w-full outline-none text-gray-700"
+                    placeholder="Search groceries..."
+                  />
+                </form>
+                <button onClick={() => setSearchBarOpen(false)}>
+                  <X className="text-gray-500 w-5 h-5" />
                 </button>
               </motion.div>
             )}
