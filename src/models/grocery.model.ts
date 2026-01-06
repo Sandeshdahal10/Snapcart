d721a90e8ffc7b562de8292095e816a2e78f7c25
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-06 08:52:49 +0545

    created interface for grocery model

--- src/models/grocery.model.ts
+++ src/models/grocery.model.ts
@@ -1,3 +1,13 @@
+import mongoose from "mongoose";
+
 interface IGrocery {
-  
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  image?: string;
+  createdAt?:Date;
+  updatedAt?:Date;
 }
+
