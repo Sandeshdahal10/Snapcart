37388e0e96138858085f9128bf714fb267dbeb02
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-06 09:02:21 +0545

    Created grocery model

--- src/models/grocery.model.ts
+++ src/models/grocery.model.ts
@@ -7,7 +7,51 @@ interface IGrocery {
   price: string;
   unit: string;
   image?: string;
-  createdAt?:Date;
-  updatedAt?:Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
+const grocerySchema = new mongoose.Schema<IGrocery>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Fruits & Vegetables",
+        "Dairy & Eggs",
+        "Rice, Atta & Grains",
+        "Snacks & Biscuits",
+        "Spices",
+        "Beverages & Drinks",
+        "Personal Care",
+        "Household Essentials",
+        "Instant & Packaged Food",
+        "Baby & Pet Care",
+      ],
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    unit: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Grocery =
+  mongoose.models.Grocery || mongoose.model("Grocery", grocerySchema);
+
+export default Grocery;
