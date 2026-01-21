8c9702c380cb6fcd6535e2003339dc4daa380c0b
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-21 14:46:01 +0545

    Added cloudinary hostname

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,9 @@ const nextConfig: NextConfig = {
       {hostname:"lh3.googleusercontent.com"},
       {hostname:"images.unsplash.com"},
       {hostname:"media.istockphoto.com"},
-      {hostname:"plus.unsplash.com"}
+      {hostname:"plus.unsplash.com"},
+      {hostname:"res.cloudinary.com"}
+
 
     ]
   }
