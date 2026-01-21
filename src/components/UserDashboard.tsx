9035678743c36fd3cca11f6968c18eb3e36c9871
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-21 14:25:41 +0545

    Wrap GroceryCard components into UserDashboard components

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from './HeroSection'
 import CategorySlider from './CategorySlider'
+import GroceryCard from './GroceryCard'
 
 function UserDashboard() {
   return (
     <>
     <HeroSection/>
     <CategorySlider/>
+    <GroceryCard/>
     </>
   )
 }
