5703d091ec54a1c161e86d5b14c950a663789759
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-22 14:12:12 +0545

    Set screen height up to minimum 200vh to scroll down further to check the components working properly or not

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-linear-to-b from-green-50 to-white w-full min-h-screen">
+      <body className="bg-linear-to-b from-green-50 to-white w-full min-h-[200vh]">
         <Provider>{children}</Provider>
       </body>
     </html>
