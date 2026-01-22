d768f87e1a423b1ed200e55ee12728c908e68d52
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-22 13:51:09 +0545

    creating category slider .

--- src/components/CategorySlider.tsx
+++ src/components/CategorySlider.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Apple, Baby, Box, Coffee, Cookie, Flame, Heart, Home, Milk, Wheat } from 'lucide-react'
+import { Apple, Baby, Box, ChevronLeft, ChevronRight, Coffee, Cookie, Flame, Heart, Home, Milk, Wheat } from 'lucide-react'
 import { color } from 'motion'
-import React from 'react'
+import React, { useRef } from 'react'
 import { motion } from 'motion/react'
 
 function CategorySlider() {
@@ -17,6 +17,12 @@ function CategorySlider() {
         {id:9,name:"Instant & Packaged Food",icon:Box,color:"bg-teal-100"},
         {id:10,name:"Baby & Pet Care",icon:Baby,color:"bg-rose-100"},
   ]
+  const scrollRef=useRef<HTMLDivElement>(null);
+  const scroll=(direction:"left"|"right")=>{
+    if(!scrollRef.current)return;
+    const scrollAmount=direction=="left"?-300:300;
+    scrollRef.current.scrollBy({left:scrollAmount,behavior:"smooth"})
+  }
   return (
     <motion.div
     className='w-[90%] md:w-[80%] mx-auto mt-10 relative '
@@ -26,9 +32,22 @@ function CategorySlider() {
     transition={{duration:0.6,ease:"easeInOut"}}
     >
       <h2 className='text-2xl md:text-3xl font-bold text-green-700 mb-6 text-center'>Shop by Category</h2>
-    <div className='flex gap-6 overflow-x-auto px-10 pb-4 scrollbar-hide scroll-smooth'>
-    
+      <button className='absolute left-0 top-1/2 -translate-y-1/2 z-10 bg-white shadow-lg hover:bg-green-100 rounded-full w-10 h-10 flex items-center justify-center transition-all' onClick={()=>scroll("left")}><ChevronLeft className='w-6 h-6 text-green-700'/></button>
+    <div className='flex gap-6 overflow-x-auto px-10 pb-4 scrollbar-hide scroll-smooth' ref={scrollRef}>
+    {categories.map((category)=>{
+      const Icon=category.icon
+      return<motion.div
+      key={category.id}
+      className={`min-w-[150px] md:min-w-[180px] flex flex-col items-center justify-center rounded-2xl ${category.color} shadow-md hover:shadow-xl transition-all cursor-pointer`}
+      >
+        <div className='flex flex-col items-center justify-center p-5'>
+        <Icon className='w-10 h-10 text-green-700 mb-3'/>
+        <p className='text-center text-sm md:text-base font-semibold text-gray-700x`'>{category.name}</p>
+        </div>
+      </motion.div>
+    })}
     </div>
+    <button className='absolute right-0 top-1/2 -translate-y-1/2 z-10 bg-white shadow-lg hover:bg-green-100 rounded-full w-10 h-10 flex items-center justify-center transition-all' onClick={()=>scroll("right")}><ChevronRight className='w-6 h-6 text-green-700'/></button>
     </motion.div>
 
   )
