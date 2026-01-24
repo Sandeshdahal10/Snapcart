3bd364d8f82b879a88616e4fbef67d3239708e16
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-24 14:57:10 +0545

    Stored cartslive in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import userSlice from "./userSlice";
+import cartSlice from "./cartSlice";
+
+
 export const store = configureStore({
   reducer: {
     user: userSlice,
+    cart: cartSlice,
   }
 })
 
