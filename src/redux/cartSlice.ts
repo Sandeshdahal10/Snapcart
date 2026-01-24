6dc9c53bcbcba1c4ef57d248ed8d124c028d2cfd
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-24 16:05:35 +0545

    Created decreased quantity reducer in cartSlice

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -14,26 +14,40 @@ interface IGrocery {
 }
 
 interface ICartSlice {
-  cartData:IGrocery[];
+  cartData: IGrocery[];
 }
 const initialState: ICartSlice = {
- cartData: [],
+  cartData: [],
 };
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    setCartData:(state, action:PayloadAction<IGrocery>) => {
+    setCartData: (state, action: PayloadAction<IGrocery>) => {
       state.cartData.push(action.payload);
     },
-    increaseQuantity:(state,action:PayloadAction<mongoose.Types.ObjectId>)=>{
-      const item=state.cartData.find(i=>i._id==action.payload)
-      if(item){
-        item.quantity=item.quantity + 1
+    increaseQuantity: (
+      state,
+      action: PayloadAction<mongoose.Types.ObjectId>,
+    ) => {
+      const item = state.cartData.find((i) => i._id == action.payload);
+      if (item) {
+        item.quantity = item.quantity + 1;
       }
-    }
+    },
+    decreaseQuantity: (
+      state,
+      action: PayloadAction<mongoose.Types.ObjectId>,
+    ) => {
+      const item = state.cartData.find((i) => i._id == action.payload);
+      if (item?.quantity && item.quantity > 1) {
+        item.quantity = item.quantity - 1;
+      } else {
+        state.cartData = state.cartData.filter((i) => i._id !== action.payload);
+      }
+    },
   },
 });
 
-export const { setCartData,increaseQuantity } = cartSlice.actions;
+export const { setCartData, increaseQuantity,decreaseQuantity } = cartSlice.actions;
 export default cartSlice.reducer;
