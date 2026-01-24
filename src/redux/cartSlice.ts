ec79866d5413926d300e853840a8dc86074f4452
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-24 15:05:09 +0545

    feat: update cart slice redux logic

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,19 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import mongoose from "mongoose";
 
+interface IGrocery {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  category: string;
+  price: string;
+  unit: string;
+  quantity: number;
+  image?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
 interface ICartSlice {
-  
+  cartData:IGrocery[];
 }
 const initialState: ICartSlice = {
- 
+ cartData: [],
 };
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    
+    setCartData:(state, action:PayloadAction<IGrocery>) => {
+      state.cartData.push(action.payload);
+    }
   },
 });
 
-export const {  } = cartSlice.actions;
+export const { setCartData } = cartSlice.actions;
 export default cartSlice.reducer;
