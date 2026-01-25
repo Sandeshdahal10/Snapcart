13fc8267ec2b633431dadc33e3ab0f81125622c1
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-25 19:01:26 +0545

    Added Back to cart navigation which push to /user/cart route.

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -1,9 +1,23 @@
+'use client'
 import React from 'react'
+import {motion} from "motion/react"
+import { ArrowLeft } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 function Checkout() {
+  const router=useRouter();
   return (
-    <div>
+    <div className='w-[92%] md:w-[80%] mx-auto py-10 relative'>
+      <motion.button
       
+      whileTap={{scale:0.96}}
+      className='absolute left-0 top-2 flex items-center gap-2 text-green-700 hover:text-green-800 font-semibold'
+      onClick={()=>router.push("/user/cart")}>
+        <ArrowLeft size={16}/>
+        <span>Back to Cart</span>
+        
+      </motion.button>
+      <h1 className='text-3xl md:text-4xl font-bold text-green-700 text-center mb-10'>Checkout</h1>
     </div>
   )
 }
