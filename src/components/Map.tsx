c579451a0937cafdeb19fbcba4dcd00d8c2c4931
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-27 20:24:04 +0545

    Integrating map from react leaflet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,21 @@
+'use client'
+import { LatLngExpression } from 'leaflet'
 import React from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import "leaflet/dist/leaflet.css"
 
 function Map({position}:{position:[number,number] | null}) {
+  if(!position)return null
   return (
-    <div>
-
-    </div>
+    <MapContainer center={position as LatLngExpression} zoom={13} scrollWheelZoom={false} className='w-full h-[400px]'>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    
+     
+    
+  </MapContainer>
   )
 }
 
