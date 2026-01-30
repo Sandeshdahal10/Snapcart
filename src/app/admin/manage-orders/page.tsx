48166d8553e3ec3086a7ae1f6b49da0dd88de85f
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-01-30 13:42:09 +0545

    Successfully fetched the data of orders

--- src/app/admin/manage-orders/page.tsx
+++ src/app/admin/manage-orders/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client'
+import { IOrder } from '@/models/order.model'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function ManageOrders() {
+  const [orders, setOrders]= useState<IOrder[]>()
+  useEffect(()=>{
+    const getOrders = async()=>{
+      try {
+        const result=await axios.get('/api/auth/admin/get-orders')
+        
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getOrders();
+  }, [])
   return (
     <div>
-      
+
     </div>
   )
 }
