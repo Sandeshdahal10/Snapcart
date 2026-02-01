a333e89a1e97b39a8eef48b3f5dcc7e75f43e0ea
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-01 16:07:44 +0545

    Fetched Order model data from backend

--- src/app/user/checkout/page.tsx
+++ src/app/user/checkout/page.tsx
@@ -34,7 +34,7 @@ const markerIcon = new L.Icon({
 function Checkout() {
   const router = useRouter();
   const { userData } = useSelector((state: RootState) => state.user);
-  const { subTotal,deliveryFee,finalTotal } = useSelector((state: RootState) => state.cart);
+  const { subTotal,deliveryFee,finalTotal,cartData } = useSelector((state: RootState) => state.cart);
   const [address, setAddress] = useState({
     fullName: "",
     mobile: "",
@@ -47,7 +47,6 @@ function Checkout() {
   const [search, setSearch] = useState("");
   const [position, setPosition] = useState<[number, number] | null>(null);
   const [paymentMethod, setPaymentMethod] = useState<"COD" | "Online">("COD");
-
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
@@ -134,6 +133,39 @@ function Checkout() {
             { enableHighAccuracy: true, maximumAge: 0, timeout: 10000 });
         },
       );
+    }
+  }
+  const handleCod=async ()=>{
+    if(!position){
+      return null
+    }
+    try {
+      const result=await axios.post("/api/user/order",{
+        userId:userData?._id,
+        items:cartData.map(item=>({
+          grocery:item._id,
+          name:item.name,
+          price:item.price,
+          unit:item.unit,
+          image:item.image,
+          quantity:item.quantity,
+        })),
+        paymentMethod: "COD",
+        totalPrice: finalTotal,
+        address:{
+          fullName: address.fullName,
+          pincode: address.pin,
+          city: address.city,
+          state: address.state,
+          mobile: address.mobile,
+          fullAddress: address.fullAddress,
+          latitude:  position[0],
+          longitude: position[1]
+        }
+      })
+      router.push("/user/order-success")
+    } catch (error) {
+      console.error(error);
     }
   }
   return (
@@ -349,7 +381,13 @@ function Checkout() {
             </div>
             <motion.button
             whileTap={{scale:0.93}}
-            className="w-full mt-6 bg-green-600 text-white py-3 rounded-full hover:bg-green-700 transition-all font-semibold">
+            className="w-full mt-6 bg-green-600 text-white py-3 rounded-full hover:bg-green-700 transition-all font-semibold" onClick={()=>{
+              if(paymentMethod=="COD"){
+                handleCod()
+              }else{
+                null
+              }
+            }}>
               {paymentMethod=="COD"?"place Order":"Proceed to Pay"}
             </motion.button>
           </div>
