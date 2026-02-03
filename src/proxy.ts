7bb91b98e7f423aab915ba79ab9abd95ebef2125
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-03 08:31:02 +0545

    Changes authentication for Stripe webhook route.

--- src/proxy.ts
+++ src/proxy.ts
@@ -58,7 +58,7 @@ export async function proxy(req:NextRequest){
   return NextResponse.next();
 }
 export const config = {
-  matcher: '/((?!api/auth|_next/static|_next/image|favicon.ico).*)',
+  matcher: '/((?!api/auth|api/user/stripe/webhook|_next/static|_next/image|favicon.ico).*)',
 }
 
 // req--------middlware-----allow-----server
