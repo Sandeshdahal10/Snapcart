36dc8fed5e5804a09955f7f1ca265adbb5bf0a48
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-04 10:17:21 +0545

    Wrapping UserOrderCard into my-orders page

--- src/app/user/my-orders/page.tsx
+++ src/app/user/my-orders/page.tsx
@@ -1,51 +1,78 @@
-'use client'
-import { IOrder } from '@/models/order.model'
-import axios from 'axios'
-import { ArrowLeft, Package } from 'lucide-react'
-import { useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+"use client";
+import UserOrderCard from "@/components/UserOrderCard";
+import { IOrder } from "@/models/order.model";
+import axios from "axios";
+import { ArrowLeft, Package } from "lucide-react";
+import { motion } from "motion/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 function MyOrder() {
-  const router= useRouter();
-  const [orders,setOrders]=useState<IOrder[]>();
-  const [loading,setLoading]=useState(true);
-  useEffect(()=>{
+  const router = useRouter();
+  const [orders, setOrders] = useState<IOrder[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
     const getMyOrders = async () => {
       try {
-        const result=await axios.get("/api/user/my-orders")
-        setOrders(result.data)
+        const result = await axios.get("/api/user/my-orders");
+        setOrders(result.data.orders);
         setLoading(false);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     getMyOrders();
-  },[])
+  }, []);
 
-if(loading){
-  return <div className='flex items-center justify-center min-h-[50vh] text-gray-600'>Loading...</div>
-}
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-[50vh] text-gray-600">
+        Loading...
+      </div>
+    );
+  }
 
   return (
-    <div className='bg-linear-to-b from white to-gray-100 min-h-screen w-full'>
-      <div className='max-w-3xl mx-auto px-4 pt-16 pb-10 relative'>
-        <div className='fixed top-0 left-0 w-full backdrop-blur-lg bg-white/70 shadow-sm border-b z-50'>
-          <div className='max-w-3xl mx-auto flex items-center gap-4 px-4 py-3'>
-            <button className='p-2 bg-gray-100 rounded-full hover:bg-gray-200 active:scale-95 transition' onClick={()=>router.push("/")}><ArrowLeft className='text-green-700' size={24} /></button>
-            <h1 className='text-xl font-bold text-gray-800'>My Orders</h1>
+    <div className="bg-linear-to-b from white to-gray-100 min-h-screen w-full">
+      <div className="max-w-3xl mx-auto px-4 pt-16 pb-10 relative">
+        <div className="fixed top-0 left-0 w-full backdrop-blur-lg bg-white/70 shadow-sm border-b z-50">
+          <div className="max-w-3xl mx-auto flex items-center gap-4 px-4 py-3">
+            <button
+              className="p-2 bg-gray-100 rounded-full hover:bg-gray-200 active:scale-95 transition"
+              onClick={() => router.push("/")}
+            >
+              <ArrowLeft className="text-green-700" size={24} />
+            </button>
+            <h1 className="text-xl font-bold text-gray-800">My Orders</h1>
           </div>
         </div>
-        {orders?.length==0? (
-          <div >
-            <Package/>
-            <h2>No orders found.</h2>
-            <p>Start shopping to place your first order.</p>
+        {orders?.length == 0 ? (
+          <div className="pt-20 flex flex-col items-center text-center">
+            <Package className="text-green-600 mb-4" size={70} />
+            <h2 className="text-xl font-semibold text-gray-700">
+              No orders found.
+            </h2>
+            <p className="text-gray-500 text-sm mt-1">
+              Start shopping to place your first order.
+            </p>
           </div>
-        ):<div></div>}
-
+        ) : (
+          <div className="mt-4 space-y-6">
+            {orders?.map((order, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3 }}
+              >
+                <UserOrderCard order={order} />
+              </motion.div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default MyOrder
+export default MyOrder;
