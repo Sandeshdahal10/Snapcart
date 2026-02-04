842886d0fb32ea277fa010db0d1bb2113b8c7e9a
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-04 13:35:25 +0545

    Added hrefadmin/manage-orders link to nav component of admin panel

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -205,7 +205,7 @@ function Nav({ user }: { user: IUser }) {
                 <Boxes className="w-5 h-5" /> View Products
               </Link>
               <Link
-                href={""}
+                href={"/admin/manage-orders"}
                 className="flex items-center gap-2 bg-white text-green-700 font-semibold px-4 py-2 rounded-full hover:bg-green-100 transition-all"
               >
                 <ClipboardCheckIcon className="w-5 h-5" /> Manage Orders
