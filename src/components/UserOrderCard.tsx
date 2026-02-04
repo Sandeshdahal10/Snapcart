75629333c122a414127b82f18272a5cbb5c32603
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-04 10:17:42 +0545

    Designing the UserOrderCard component to display individual user orders in the my order page.

---
+++ src/components/UserOrderCard.tsx
@@ -0,0 +1,13 @@
+'use client'
+import { IOrder } from '@/models/order.model'
+import React from 'react'
+
+function UserOrderCard({order}: {order:IOrder}) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default UserOrderCard
