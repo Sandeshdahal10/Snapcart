a7976e29980540e0897788eedfea34a4a5eb6246
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-05 17:11:46 +0545

    implementing connection lost and established through socket.io

--- socketServer/index.js
+++ socketServer/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import http from 'http';
 import dotenv from 'dotenv';
+import { Server } from 'socket.io';
 
 dotenv.config();
 const app=express();
@@ -8,6 +9,17 @@ const app=express();
 
 const server=http.createServer(app);
 const port=process.env.PORT||5000;
+const socketIo=new Server(server,{
+  cors:{
+    origin:process.env.NEXT_BASE_URL,
+  }
+})
+socketIo.on("Connection",(socket)=>{
+  console.log("User Connected",socket.id);
+  socket.on("Disconnect",()=>{
+    console.log("User Disconnected",socket.id);
+  })
+})
 server.listen(port,()=>{
     console.log(`Socket server is running on port ${port}`);
 })
