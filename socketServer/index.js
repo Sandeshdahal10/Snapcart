61a0d72963a2d0de9866c5b602f1aa83b681f6d8
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-05 16:58:21 +0545

    Created the listening port of the server

--- socketServer/index.js
+++ socketServer/index.js
@@ -1,2 +1,13 @@
 import express from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 const app=express();
+
+
+const server=http.createServer(app);
+const port=process.env.PORT||5000;
+server.listen(port,()=>{
+    console.log(`Socket server is running on port ${port}`);
+})
