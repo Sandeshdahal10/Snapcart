6a663cb9c93d17cbfdef664de70adcd83da64fc2
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-07 07:48:50 +0545

    Fixed the bug of server code of socket.

--- socketServer/index.js
+++ socketServer/index.js
@@ -2,21 +2,22 @@ import express from 'express';
 import http from 'http';
 import dotenv from 'dotenv';
 import { Server } from 'socket.io';
-
+console.log("socket file loading...")
 dotenv.config();
 const app=express();
 
 
 const server=http.createServer(app);
 const port=process.env.PORT||5000;
-const socketIo=new Server(server,{
+const io=new Server(server,{
   cors:{
     origin:process.env.NEXT_BASE_URL,
+    methods:["GET","POST"]
   }
 })
-socketIo.on("Connection",(socket)=>{
+io.on("connection",(socket)=>{
   console.log("User Connected",socket.id);
-  socket.on("Disconnect",()=>{
+  socket.on("disconnect",()=>{
     console.log("User Disconnected",socket.id);
   })
 })
