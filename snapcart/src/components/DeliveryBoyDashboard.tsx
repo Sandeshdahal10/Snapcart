ff21b5ef6a80843f3c4caa95d609b672a3f27e2c
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-14 17:28:24 +0545

    Successfully fetched the /api/delivery/get-assign ments in DeliveryBoyDashboard.tsx

--- snapcart/src/components/DeliveryBoyDashboard.tsx
+++ snapcart/src/components/DeliveryBoyDashboard.tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+'use client';
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function DeliveryBoyDashboard() {
+  useEffect(()=>{
+    const fetchAssignments=async()=>{
+      try {
+        const result=await axios.get('/api/delivery/get-assignments');
+        console.log(result);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchAssignments();
+  },[])
   return (
     <div>
       
