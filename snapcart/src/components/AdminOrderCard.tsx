ddead3b218a00c34d9fcda09d3bff0ae96f429c9
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-14 15:11:57 +0545

    Changes status to a state variable to reflect changes immediately on the UI without needing to refresh the page.

--- snapcart/src/components/AdminOrderCard.tsx
+++ snapcart/src/components/AdminOrderCard.tsx
@@ -18,10 +18,12 @@ import React, { use, useState } from "react";
 function AdminOrderCard({ order }: { order: IOrder }) {
   const status = ["Pending", "Out for Delivery"];
   const [expanded, setExpanded] = useState(false);
+  const [orderStatus,setOrderStatus]=useState<string>(order.status);
   const updateStatus=async (orderId:string,status:string)=>{
     try {
       const result= await axios.post(`/api/auth/admin/update-order-status/${orderId}`,{status})
       console.log(result.data);
+      setOrderStatus(status);
     } catch (error) {
       console.log(error);
     }
@@ -79,16 +81,17 @@ function AdminOrderCard({ order }: { order: IOrder }) {
         <div className="flex flex-col items-start md:items-end gap-2">
           <span
             className={`text-xs font-semibold px-3 py-1 rounded-full capitalize ${
-              order.status === "Delivered"
+              orderStatus === "Delivered"
                 ? "bg-green-100 text-green-700"
-                : order.status === "Pending"
+                : orderStatus === "Pending"
                   ? "bg-yellow-100 text-yellow-700"
                   : "bg-blue-100 text-blue-700"
             }`}
           >
-            {order.status}
+            {orderStatus}
           </span>
           <select className="border border-gray-300 rounded-lg px-3 py-1 text-sm shadow-sm hover:border-green-400 transition focus:ring-2 focus:ring-green-500 outline-none"
+          value={orderStatus}
           onChange={(e)=>updateStatus(order._id?.toString()!,e.target.value)}
           >
             {status.map((stat) => (
@@ -159,7 +162,7 @@ function AdminOrderCard({ order }: { order: IOrder }) {
           <span>
             Delivery:{" "}
             <span className="text-green-700 font-bold">
-              {order.status}
+              {orderStatus}
             </span>{" "}
           </span>
         </div>
