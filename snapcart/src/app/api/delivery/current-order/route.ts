00fb0b899ff5f3d44f11a7c511e6c825b0bf692d
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-17 08:15:59 +0545

    Created the route /api/delivery/current-order to fetch the current order assigned to the delivery boy.

---
+++ snapcart/src/app/api/delivery/current-order/route.ts
@@ -0,0 +1,31 @@
+import { auth } from "@/auth";
+import connectDb from "@/lib/db";
+import DeliveryAssignment from "@/models/deliveryAssignment.model";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connectDb();
+    const session = await auth();
+    const deliveryBoyId = session?.user?.id;
+    const activeAssignment = await DeliveryAssignment.findOne({
+      assignedTo: deliveryBoyId,
+      status: "assigned",
+    })
+      .populate({ path: "order", populate: { path: "address" } })
+      .lean();
+    if (!activeAssignment) {
+      return NextResponse.json({ active: false }, { status: 200 });
+    } else {
+      return NextResponse.json(
+        { active: true, assignment: activeAssignment },
+        { status: 200 },
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch current order" },
+      { status: 500 },
+    );
+  }
+}
