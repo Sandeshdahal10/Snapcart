3312647cb8efd997cb2056d158ddb419fa02d943
Sandesh Dahal <sandeshdahal3547@gmail.com>
2026-02-18 16:04:55 +0545

    Created useEffect to send live location of delivery boy.

--- snapcart/src/components/DeliveryBoyDashboard.tsx
+++ snapcart/src/components/DeliveryBoyDashboard.tsx
@@ -6,11 +6,23 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import LiveMap from "./LiveMap";
 
+interface Ilocation {
+  latitude: Number;
+  longitude: Number;
+}
 function DeliveryBoyDashboard() {
   const [assignments, setAssignments] = useState<any[]>([]);
   const { userData } = useSelector((state: RootState) => state.user);
   const [activeOrder, setActiveOrder] = useState<any>(null);
-  const [userLocation,setUserLocation]=useState<any>(null)
+  const [userLocation, setUserLocation] = useState<Ilocation>({
+    latitude: 0,
+    longitude: 0,
+  });
+  const [deliveryBoyLocation, setDeliveryBoyLocation] = useState<Ilocation>({
+    latitude: 0,
+    longitude: 0,
+  });
+
   const fetchAssignments = async () => {
     try {
       const result = await axios.get("/api/delivery/get-assignments");
@@ -39,12 +51,12 @@ function DeliveryBoyDashboard() {
   const fetchCurrentOrder = async () => {
     try {
       const result = await axios.get("/api/delivery/current-order");
-      if(result.data.active){
+      if (result.data.active) {
         setActiveOrder(result.data.assignment);
         setUserLocation({
           latitude: result.data.assignment.order.address.latitude,
           longitude: result.data.assignment.order.address.longitude,
-        })
+        });
       }
     } catch (error) {
       console.log(error);
@@ -55,18 +67,52 @@ function DeliveryBoyDashboard() {
     fetchCurrentOrder();
   }, [userData]);
 
-  if(activeOrder && userLocation){
+  useEffect(() => {
+    const socket = getSocket();
+    if (userData?._id) {
+      if (!navigator.geolocation) return;
+      const watcher = navigator.geolocation.watchPosition(
+        (pos) => {
+          const lat = pos.coords.latitude;
+          const lon = pos.coords.longitude;
+          setDeliveryBoyLocation({
+            latitude: lat,
+            longitude: lon,
+          });
+          socket.emit("update-location", {
+            userId: userData?._id,
+            latitude: lat,
+            longitude: lon,
+          });
+        },
+        (err) => {
+          console.log(err);
+        },
+        { enableHighAccuracy: true },
+      );
+      return () => navigator.geolocation.clearWatch(watcher);
+    }
+  }, [userData?._id]);
+
+  if (activeOrder && userLocation) {
     return (
       <div className="p-4 pt-[120px] min-h-screen bg-gray-50">
         <div className="max-w-3xl mx-auto">
-          <h1 className="text-2xl font-bold text-green-700 mb-2">Active Delivery</h1>
-          <p className="text-gray-600 text-sm mb-4">Active order #{activeOrder?.order?._id.slice(-6)}</p>
+          <h1 className="text-2xl font-bold text-green-700 mb-2">
+            Active Delivery
+          </h1>
+          <p className="text-gray-600 text-sm mb-4">
+            Active order #{activeOrder?.order?._id.slice(-6)}
+          </p>
           <div className="rounded-xl border shadow-lg overflow-hidden mb-6">
-            <LiveMap/>
+            <LiveMap
+              userLocation={userLocation}
+              deliveryBoyLocation={deliveryBoyLocation}
+            />
           </div>
         </div>
       </div>
-    )
+    );
   }
   return (
     <div className="w-full min-h-screen bg-gray-50 p-4">
